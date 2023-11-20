2a771675b680253de9fbd7d384732f44af1ba24a
ciliaence <cilia.ence@telia.com>
2023-11-20 12:29:51 +0100

    added navigation, shared pref.

---
+++ lib/add_todo.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AddTodoPage extends StatelessWidget {
+  const AddTodoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add Todo'),
+      ),
+    );
+  }
+}
